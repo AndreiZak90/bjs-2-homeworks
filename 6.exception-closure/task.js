9b59cdb97bc0197605b8902a1049d26d8be67b64
AndreiZak <zak-90@inbox.ru>
2023-04-18 23:14:27 +0300

    exception commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(meaning) {
+  const pars = Number.parseFloat(meaning);
+  if (Number.isNaN(pars)) {
+    throw new Error("Невалидное значение");
+  }
+  return pars;
+}
+
+function validateCount(meaningNew) {
+  try {
+    return parseCount(meaningNew);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(left, right, down) {
+    this.left = left;
+    this.right = right;
+    this.down = down;
+    if (left + right < down || left + down < right || right + down < left) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+    return this.left + this.right + this.down;
+  }
+
+  get area() {
+    let per = this.perimeter / 2;
+    return +Math.sqrt(
+      per * (per - this.left) * (per - this.right) * (per - this.down)
+    ).toFixed(3);
+  }
+}
+
+function getTriangle(left, right, down) {
+  try {
+    return new Triangle(left, right, down);
+  } catch {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
